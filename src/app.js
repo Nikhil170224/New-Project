a699a2c011e4a1974d3c795204c7735e03d1c850
Nikhil170224 <nikhilmi1050@gmail.com>
2026-01-11 18:15:52 +0530

    saved 1st user document into database using post api call

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./model/user");
 
+app.post("/signup", async(req, res) => {
+    const userObj = new User({
+        firstName: "Virat",
+        lastName: "Kohli",
+        emailId: "virat@kohli.com",
+        age: "28",
+        gender: "male"
+    })
 
+    try {
+        await userObj.save()
+        res.send("User Data saved Succesfully!!")
+    } catch (err) {
+        res.status(400).send("error in saving the user:" + err.message)
+    }
+})
 connectDB() 
     .then(() => {
         console.log("Database is succesfully connected to our app!!");
