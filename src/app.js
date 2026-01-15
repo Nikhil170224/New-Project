af1779c2b0ff31ae0fd977092baaea486dc274bd
Nikhil170224 <nikhilmi1050@gmail.com>
2026-01-16 03:42:02 +0530

    post,find,delete, update mongoose methods to create APIs

--- src/app.js
+++ src/app.js
@@ -3,14 +3,9 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./model/user");
 
-app.post("/signup", async(req, res) => {
-    const userObj = new User({
-        firstName: "Virat",
-        lastName: "Kohli",
-        emailId: "virat@kohli.com",
-        age: "28",
-        gender: "male"
-    })
+app.use(express.json());
+app.post("/signup", async (req, res) => {
+    const userObj = new User(req.body);
 
     try {
         await userObj.save()
@@ -19,6 +14,37 @@ app.post("/signup", async(req, res) => {
         res.status(400).send("error in saving the user:" + err.message)
     }
 })
+
+app.get("/user", async (req, res) => {
+    const EmailId = req.body.emailId;
+
+    try {
+        const user = await User.find({ emailId: EmailId, lastName: req.body.lastName, firstName: req.body.firstName })
+        if (user.length === 0) res.status(404).send("user not found");
+        res.send(user);
+    } catch (err) {
+        res.status(400).send("something went wrong !!");
+    }
+})
+
+app.get("/userById", async (req, res) => {
+    try {
+        const user = await User.findById(req.body._id);
+        res.send(user);
+    } catch (err) {
+        res.status(400).send("something went wrong !!");
+    }
+})
+
+app.delete("/user", async (req, res) => {
+    try {
+        await User.deleteOne({ firstName: req.body.firstName })
+        res.send("user deleted successfully")
+    } catch {
+        res.status(400).send("something went wrong !!");
+    }
+})
+
 connectDB() 
     .then(() => {
         console.log("Database is succesfully connected to our app!!");
